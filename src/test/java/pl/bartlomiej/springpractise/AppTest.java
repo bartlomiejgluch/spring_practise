72d2e2032623ab6e7a0d59d94ba90c37196b7794
bartlomiej <bartlomiejgluch@gmail.com>
2019-01-02 10:59:45 +0100

    Initial commit, spring configuration

---
+++ src/test/java/pl/bartlomiej/springpractise/AppTest.java
@@ -0,0 +1,20 @@
+package pl.bartlomiej.springpractise;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
